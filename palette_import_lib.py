ce95df5863c1c2006b27073ed9cd162603fc324c
boyuch <boyuchen2001@gmail.com>
2018-12-07 10:37:19 -0500

    palette_import completed

--- palette_import_lib.py
+++ palette_import_lib.py
@@ -1,13 +1,15 @@
-"""
-functions to read the a CSV file
-and put it into a dictionary
-"""
-
+import os
+import csv
 
 def read_palette_from_file(palette_filename):
-    """
-    read the palette from the csv file.
-    Each row is a key and an RGB triple.  Make that RGB triple
-    into a TUPLE and then put into a dictionary with the key.
-    """
-    pass
+    my_dict = dict()
+    my_dir = os.path.dirname(os.path.realpath(__file__))
+    palette_filename = 'CHROMA.MAP.csv'
+    with open (my_dir+'/'+palette_filename,'r') as f:
+        csv_reader = csv.reader(f)
+        for row in csv_reader:
+            key = int(row[0])
+            value = (int(row[1]),int(row[2]),int(row[3]))
+            my_dict[key] = value
+    print (my_dict)
+    return my_dict
